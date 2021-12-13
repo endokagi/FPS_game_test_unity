7487dbb9788f2783240403f4a5d7655ba1f6af23
Witthawat  Tanoi <38246228+endokagi@users.noreply.github.com>
2021-12-13 10:48:58 +0700

    update

--- Assets/Scripts/AIm.cs
+++ Assets/Scripts/AIm.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class AIm : MonoBehaviour
 {
 
-    // Object 
+    public Transform GunContainer, ADS;
+
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,19 @@ public class AIm : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButton(1)) Ads();
+        if(!Input.GetMouseButton(1)) non_Ads();
+    }
+
+    private void Ads(){
+        transform.SetParent(ADS);   
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+    }
+
+    private void non_Ads(){
+        transform.SetParent(GunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
     }
 }
